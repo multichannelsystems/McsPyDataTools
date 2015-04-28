92cbb4d1eba62a324a3939cd4819d860a287d2e6
Hans Löffler <loeffler@multichannelsystems.com>
2015-04-28 13:08:19 +0200

    test python filer enamed

--- McsPyTutorial/DataStreamInfo.py
+++ McsPyTutorial/DataStreamInfo.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env/python
+# -*- coding: utf-8 -*-
 
 import os
 import McsPy
 import McsPy.McsData
 import argparse
-#from TableFormatter import TableFormatter
+import datetime
 from tabulate import tabulate
 
 
-test_raw_data_file_path = r"TestData\mcd_Tutorial\1,2.h5"
-test_raw_data_file_path = r"TestData\mcd_Tutorial\Neuro_OTC_Spikes_Demo.h5"    
-test_raw_data_file_path = r"TestData\mcd_Tutorial\PPF_Data.h5"    
-test_raw_data_file_path = r"TestData\mcd_Tutorial\LinearLayout_StandardRecording.h5"
+# test_raw_data_file_path = r"TestData\mcd_Tutorial\1,2.h5"
+# test_raw_data_file_path = r"TestData\mcd_Tutorial\Neuro_OTC_Spikes_Demo.h5"
+# test_raw_data_file_path = r"TestData\mcd_Tutorial\PPF_Data.h5"
+# test_raw_data_file_path = r"TestData\mcd_Tutorial\LinearLayout_StandardRecording.h5"
 
 
 def print_analog_channel_info(streams):
@@ -40,24 +41,40 @@ def print_timestamp_channel_info(streams):
 
 
 def print_header_info(h5filename, raw_data):
-    print("filename: " + h5filename)
-    print("date: {0}".format(raw_data.date))
-    print("program name: " + raw_data.program_name)
-    print("program version: " + raw_data.program_version) 
+    print("")
+    print(h5filename)
+    print("")
+    t_row = []
+    d = datetime.datetime(1, 1, 1) + datetime.timedelta(microseconds=int(raw_data.date_in_clr_ticks)/10)
+    t_row.append(str(d.strftime("%Y-%m-%d %H:%M:%S")))
+    t_row.append(raw_data.program_name)
+    t_row.append(raw_data.program_version)
+    real_row = []
+    real_row.append(t_row)
+    table_header = ["Date", "Program", "Version"]
+    print(tabulate(real_row, headers=table_header))
 
 
 def get_number_of_streams(rec, stream_type):
     num_streams = 0
     try:
         if stream_type == "analog":
-            num_streams = len(rec.analog_streams)
+            if rec.analog_streams != None:
+                num_streams = len(rec.analog_streams)
         if stream_type == "event":
-            num_streams = len(rec.event_streams)
+            if rec.event_streams != None:
+                num_streams = len(rec.event_streams)
         if stream_type == "segment":
-            num_streams = len(rec.segment_streams)
+            if rec.segment_streams != None:
+                num_streams = len(rec.segment_streams)
         if stream_type == "timestamp":
-            num_streams = len(rec.timestamp_streams)
-    except KeyError:
+            if rec.timestamp_streams != None:
+                num_streams = len(rec.timestamp_streams)
+    except KeyError, e:
+        print(e.message)
+        num_streams = 0
+    except ValueError, e:
+        print(e.message)
         num_streams = 0
     return num_streams
 
@@ -65,6 +82,43 @@ def get_number_of_streams(rec, stream_type):
 def print_number_of_streams(rec, stream_type):
     num_streams = get_number_of_streams(rec, stream_type)
     print("number of {} streams: {}".format(stream_type, num_streams))
+
+
+def get_streams_of_type(rec, stream_type):
+    if stream_type == "analog":
+        return rec.analog_streams
+    elif stream_type == "event":
+        return rec.event_streams
+    elif stream_type == "segment":
+        return rec.segment_streams
+    elif stream_type == "timestamp":
+        return rec.timestamp_streams
+    else:
+        assert False, "unknown stream type"
+        return None
+
+
+def get_info_rows(streams):
+    rows = []
+    x = streams.iteritems()
+    for id, s in x:
+        row = []
+        row.append(s.stream_type)
+        row.append(s.label)
+        try:
+            l = len(s.channel_infos)
+            row.append(l)
+        except AttributeError:
+            row.append("")
+        rows.append(row)
+    return rows
+
+
+def get_stream_info_rows(rec, stream_type):
+    num_streams = get_number_of_streams(rec, stream_type)
+    if num_streams > 0:
+        rows = get_info_rows(get_streams_of_type(rec, stream_type))
+        return rows
 
 
 def print_stream_info(rec):
@@ -78,7 +132,6 @@ def print_stream_info(rec):
     print_number_of_streams(rec, stream_type)
     if num_streams > 0:
         print_analog_channel_info(rec.analog_streams)
-
 
     stream_type = "event"
     num_streams = get_number_of_streams(rec, stream_type)
@@ -98,41 +151,35 @@ def print_stream_info(rec):
     if num_streams > 0:
         print_timestamp_channel_info(rec.event_streams)
 
-    #try:
-    #    num_streams = len(rec.event_streams)
-    #except KeyError:
-    #    num_streams = 0
-    #print("number of event streams: {}".format(num_streams))
-    #if num_streams > 0:
-    #    print_event_channel_info(rec.event_streams)
-
 
 def print_file_info(h5filename):
 
-    McsPy.McsData.verbose = False
+    McsPy.McsData.VERBOSE = False
 
     raw_data = McsPy.McsData.RawData(h5filename)
     print_header_info(h5filename, raw_data)
-
     recording = raw_data.recordings[0]
-
     print_stream_info(recording)
 
-#    print("number of analog streams: {}".format(len(raw_data.recordings[0].analog_streams)))
-    #num_streams = len(raw_data.recordings[0].analog_streams)
 
-    #print(num_streams)
+def print_file_info2(h5filename):
 
-    #print_analog_stream_info(raw_data.recordings[0].analog_streams)
+    McsPy.McsData.VERBOSE = False
+    raw_data = McsPy.McsData.RawData(h5filename)
+    print_header_info(h5filename, raw_data)
+    print("")
 
-    #print("number of event streams")
-    #try:
-    #    num_streams = len(raw_data.recordings[0].event_streams)
-    #    print(num_streams)
-    #except KeyError:
-    #    print("no event stream found")
+    recording = raw_data.recordings[0]
+    all_rows = []
+    for n in get_stream_type_names():
+        if recording != None:
+            rows = get_stream_info_rows(recording, n)
+            if rows != None:
+                for r in rows:
+                    all_rows.append(r)
 
-    #for s in raw_data.recordings[0].analog_streams
+    table_header = ["Type", "Stream", "# ch"]
+    print(tabulate(all_rows, headers=table_header))
 
 #def tab_print():
 #    print "\n".join (map (lambda (x, y): "%s\t%s" % ("\t".join (x), y), mylist) )
@@ -140,6 +187,7 @@ def print_file_info(h5filename):
 #def test():
 #    raw_data = McsPy.McsData.RawData(test_raw_data_file_path)
 #    print(raw_data.date)   
+
 
 def parse_arguments():
     global parser
@@ -154,25 +202,36 @@ def get_directory(args):
     return args.directory
 
 
+def get_stream_type_names():
+    stream_types = ["analog", "event", "segment", "timestamp"]
+    return stream_types
+
+
 def get_table_stream_info(recording):
     info = []
-    stream_types = ["analog", "event", "segment", "timestamp"]
-    for stream_type in stream_types:
+    for stream_type in get_stream_type_names():
         num_streams = get_number_of_streams(recording, stream_type)
         info.append(str(num_streams))
     return info
+
 
 def get_table_row(f):
     try:
         raw_data = McsPy.McsData.RawData(f)
         row = []
         row.append(os.path.basename(f))
-        d = datettime.str
-        # row.append("{0}".format(raw_data.date))
-        recording = raw_data.recordings[0]
-        l = get_table_stream_info(recording)
-        for i in l:
-            row.append(i)
+        if raw_data.recordings == None:
+            row.append("Error: no raw data")
+            # append empty columns
+            for i in range(len(get_stream_type_names())):
+                row.append("")
+        else:
+            d = datetime.datetime(1, 1, 1) + datetime.timedelta(microseconds=int(raw_data.date_in_clr_ticks)/10)
+            row.append(d.strftime("%Y-%m-%d %H:%M:%S"))
+            recording = raw_data.recordings[0]
+            l = get_table_stream_info(recording)
+            for i in l:
+                row.append(i)
         return row
     except IOError, e:
         print("IOError")
@@ -180,7 +239,7 @@ def get_table_row(f):
         exit(1)
 
 def print_dir_file_info(h5files):
-    McsPy.McsData.verbose = False
+    McsPy.McsData.VERBOSE = False
 
     table_header = ["File", "Date", "Anal.", "Ev", "Seg.", "TS"]
     # tf = TableFormatter(table_header)
@@ -188,8 +247,6 @@ def print_dir_file_info(h5files):
     for f in h5files:
         row = get_table_row(f)
         table.append(row)
-    #     tf.addDataColumns(row)
-    # print(tf.createTable())
     print(tabulate(table, headers=table_header))
 
 
@@ -209,8 +266,7 @@ def data_stream_info():
 
     if args.file != None:
         filepath = os.path.join(file_dir, args.file)
-        #print("printing file info")
-        print_file_info(filepath)
+        print_file_info2(filepath)
     elif file_dir != "":
         files = os.listdir(unicode(file_dir))
         only_files = [ f for f in files if os.path.isfile(os.path.join(unicode(file_dir), f)) ]
@@ -218,17 +274,8 @@ def data_stream_info():
         if len(only_files) == 0:
             print("no files found in " + file_dir)
         else:
-            # print("available files")
-            # print(onlyfiles)
-            # print(os.path.join(file_dir, onlyfiles[0]))
-
             only_files = [os.path.join(unicode(file_dir), f) for f in only_files]
             print_dir_file_info(only_files)
 
-            # for f in only_files:
-            #     print_file_info(f)
-            #     #print_dir_file_info(os.path.join(file_dir, f))
-
 if __name__ == "__main__":
     data_stream_info()
-    #print_file_info(test_raw_data_file_path)
